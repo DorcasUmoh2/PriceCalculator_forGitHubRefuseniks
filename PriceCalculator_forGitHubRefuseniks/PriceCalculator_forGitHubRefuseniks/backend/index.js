9c507d64b86f9b0456b2a2fa3231b31c9b77df86
Your Name <you@example.com>
2025-02-18 15:01:22 +0000

    Getteing the database

--- PriceCalculator_forGitHubRefuseniks/PriceCalculator_forGitHubRefuseniks/backend/index.js
+++ PriceCalculator_forGitHubRefuseniks/PriceCalculator_forGitHubRefuseniks/backend/index.js
@@ -4,6 +4,11 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://127.0.0.1:27017";
+
+
+
 var options = {
     index: "myWebPage.html"
   };
@@ -31,6 +36,31 @@ app.get('/api/getPrice', function(req, res){
     roundedPrice = Math.round((price+roundToNearest)/roundToNearest) * roundToNearest // Always round up
     res.send(""+roundedPrice)
 });
+ // Database stuff
+    // Create a new MongoClient
+    const client = new MongoClient(uri);
+    async function run() {
+    try {
+        //Write databse Insert/Update/Query code here..
+        var dbo = client.db("mydb");
+        var myobj = { quoteName: n, salary: s, days: d }; //******CHECK!!!****
+        await dbo.collection("quotes").insertOne(myobj, function(err, res) {
+            if (err) {
+                console.log(err); 
+                throw err;
+            }
+            console.log("1 quote inserted");
+        }); 
+        console.log('End the database stuff');
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+    }
+    run().catch(console.dir);
+
+
 
 app.use(express.static(dir, options));
 
